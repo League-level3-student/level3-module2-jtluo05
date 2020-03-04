e3027851c14db4faff79908dca9055f761b4bc39
League Student <league@iMac-21.attlocal.net>
2020-03-03 19:52:30 -0800

    search algo

--- src/_01_Searching_Algorithms/_00_LinearSearch.java
+++ src/_01_Searching_Algorithms/_00_LinearSearch.java
@@ -10,6 +10,12 @@ public class _00_LinearSearch {
 		//   where the value was found.
 		//   If the value is not found in the array, return -1.
 		
-		return 0;
+		for (int i = 0; i < words.length; i++) {
+			if (value==words[i]) {
+				return i;
+			}
+			
+		}
+		return -1;	
 	}
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -7,22 +7,25 @@ import org.junit.jupiter.api.Test;
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+	int[] arr =new int[]{0,1,2,3,4,5,6};
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		//assertEquals(4,_00_LinearSearch.linearSearch(arr, "8"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		//assertEquals(5,_01_BinarySearch.binarySearch(arr,0,arr.length-1,5));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		assertEquals(5,_02_InterpolationSearch.interpolationSearch(arr,5));
 	}
 	
 	@Test
